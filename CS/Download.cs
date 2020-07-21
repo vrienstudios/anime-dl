d730feffeab6c1ab045454870f8fde2bef7bce64
Test <my-name@chromium.org>
2020-07-21 18:52:18 -0400

    DownloadCS

---
+++ CS/Download.cs
@@ -0,0 +1,18 @@
+
+public static class Download
+{
+	private static void MergeTS(string path)
+    {
+        // TODO
+    }
+
+    private static void DownloadTS(string tsListFile)
+    {
+        // TODO
+    }
+
+    public static void DownloadVideo(string Uri)
+    {
+        // TODO
+    }
+}
--- CS/Program.cs
+++ CS/Program.cs
@@ -203,6 +203,7 @@ namespace VidStreamIORipper
                 }
             }
         }
+
         ~Program()
         {
             client.Dispose();
