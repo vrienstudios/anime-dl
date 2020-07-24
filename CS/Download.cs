6f7e2f96946ac31c40d4044a38a8b9bcfea2706d
Chay <visionistprojects@gmail.com>
2020-07-24 18:55:16 -0400

    More implementations

--- CS/Download.cs
+++ CS/Download.cs
@@ -1,3 +1,4 @@
+using mshtml;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,8 +14,18 @@ namespace VidStreamIORipper
     public static class Download
     {                       // sub.8.360.m3u8
         private static Regex reg = new Regex(@"(sub\..*?\..*?\.m3u8)");
+        private static Regex dwnldLink = new Regex("(?<=\"file\":\")(.+?)(?=\")");
         private static MatchCollection match;
+        private static String directUri = string.Empty;
         private static String content = string.Empty;
+
+        private static String m3u8Manifest = string.Empty;
+        private static Boolean setM3Man(string cont)
+        {
+            m3u8Manifest = cont;
+            return true;
+        }
+
         private static void MergeTS(string path)
         {
             // TODO
@@ -25,41 +36,50 @@ namespace VidStreamIORipper
             // TODO
         }
 
-        private static String GetM3u8Link(string linktomanifest) => (match = reg.Matches(content = Program.wc.DownloadString(linktomanifest))) != null ? (match.Count > 0) ? $"{GetUrl(content)}{GetHighestRes(match.GetEnumerator())}" : null : null;
+        private static Boolean GetM3u8Link(string linktomanifest) => (match = (!linktomanifest.Contains("ajax")) ? reg.Matches(content = Program.wc.DownloadString(linktomanifest)) : reg.Matches(content = Program.wc.DownloadString(directUri = dwnldLink.Match(Program.wc.DownloadString(linktomanifest)).Groups[1].Value.Replace("\\", string.Empty)))) != null ? (match.Count > 0) ? setM3Man(Program.wc.DownloadString($"{((directUri != string.Empty) ? directUri.TrimToSlash() : content)}{GetHighestRes(match.GetEnumerator())}")) != false ? DownloadVideo() : false : false : false;
 
         private static String GetHighestRes(IEnumerator enumerator)
         {
             int current = 0;
+            string bi = string.Empty;
             string bf = string.Empty;
-            enumerator.MoveNext(); // First step should be nil, at least it is in CLI
+            //enumerator.MoveNext(); // First step should be nil, at least it is in CLI
             while(enumerator.MoveNext())
             {
-                int ia = (int.Parse(bf.Split('.')[2]) > current) ? int.Parse(bf.Split('.')[2]) : -1;
+                bf = enumerator.Current.ToString();
+                int ia = (int.Parse(bf.Split('.')[2]) > current) ? current = int.Parse(bf.Split('.')[2]) : -1;
                 switch (ia)
                 {
                     case -1: // not higher break;
-                            break;
+                        continue;
                     default:
                         {
                             current = ia;
                             ia = 0;
-                            break;
+                            bi = bf;
+                            continue;
                         }
                 }
             }
 
-            return null;
+            return bf;
         }
 
         private static String GetUrl(string fileuri)
         {
             // Todo: 
-            return null;
+            return fileuri;
         }
 
-        public static void DownloadVideo(string Uri)
+        public static Boolean DownloadVideo()
         {
             // TODO
+            return true;
+        }
+
+        public static void m3u8Test(string mfl)
+        {
+            Console.WriteLine(GetM3u8Link(mfl));
         }
 
         public static void regTest(string str)
