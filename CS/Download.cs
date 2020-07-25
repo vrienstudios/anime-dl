01d7b408f72063555e8099e77be3fbe056f1689f
Chay <visionistprojects@gmail.com>
2020-07-24 21:40:00 -0400

    Working, don't touch.

--- CS/Download.cs
+++ CS/Download.cs
@@ -7,11 +7,12 @@ using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace VidStreamIORipper
 {
-    public static class Download
+    public static class Dwnl
     {                       // sub.8.360.m3u8
         private static Regex reg = new Regex(@"(sub\..*?\..*?\.m3u8)");
         private static Regex dwnldLink = new Regex("(?<=\"file\":\")(.+?)(?=\")");
@@ -19,23 +20,16 @@ namespace VidStreamIORipper
         private static String directUri = string.Empty;
         private static String content = string.Empty;
         private static String m3u8Manifest = string.Empty;
+
+        public static String FileDest = string.Empty;
+
         private static Boolean setM3Man(string cont)
         {
             m3u8Manifest = cont;
             return true;
         }
 
-        private static void MergeTS(string path)
-        {
-            // TODO
-        }
-
-        private static void DownloadTS(string tsListFile)
-        {
-            // TODO
-        }
-
-        private static Boolean GetM3u8Link(string linktomanifest) => (match = (!linktomanifest.Contains("ajax")) ? reg.Matches(content = Program.wc.DownloadString(linktomanifest)) : reg.Matches(content = Program.wc.DownloadString(directUri = dwnldLink.Match(Program.wc.DownloadString(linktomanifest)).Groups[1].Value.Replace("\\", string.Empty)))) != null ? (match.Count > 0) ? setM3Man(Program.wc.DownloadString($"{((directUri != string.Empty) ? directUri.TrimToSlash() : content)}{GetHighestRes(match.GetEnumerator())}")) != false ? DownloadVideo() : false : false : false;
+        public static Boolean GetM3u8Link(string linktomanifest) => (match = (!linktomanifest.Contains("ajax")) ? reg.Matches(content = Program.wc.DownloadString(linktomanifest)) : reg.Matches(content = Program.wc.DownloadString(directUri = dwnldLink.Match(Program.wc.DownloadString(linktomanifest)).Groups[1].Value.Replace("\\", string.Empty)))) != null ? (match.Count > 0) ? setM3Man(Program.wc.DownloadString($"{((directUri != string.Empty) ? directUri.TrimToSlash() : (directUri = linktomanifest.TrimToSlash()) != null ? directUri : throw new Exception("Unknown Error"))}{GetHighestRes(match.GetEnumerator())}")) != false ? DownloadVideo() : throw new Exception("Error getting video information") : false : false;
 
         private static String GetHighestRes(IEnumerator enumerator)
         {
@@ -70,7 +64,9 @@ namespace VidStreamIORipper
             return fileuri;
         }
 
-        public static Boolean DownloadVideo()
+        private const int BUFFER_SIZE = 128 * 1024;
+
+        private static Boolean DownloadVideo()
         {
             String[] broken = m3u8Manifest.Split(new[] { "\r\n", "\n", "\r" }, StringSplitOptions.None);
             String path = directUri.TrimToSlash();
@@ -84,7 +80,8 @@ namespace VidStreamIORipper
                         }
                     default:
                         {
-                            mergeToMain(downloadPart($"{path}{broken[idx]}"));
+                            Console.WriteLine($"Downloading: {broken[idx]}");
+                            mergeToMain(FileDest, downloadPart($"{path}{broken[idx]}"));
                             break;
                         }
                 }
@@ -92,22 +89,35 @@ namespace VidStreamIORipper
             return true;
         }
 
-        private static Boolean mergeToMain(String partPath)
+        private static Boolean mergeToMain(String destinationFile, String partPath)
         {
+            Stream stream = new FileStream(partPath, FileMode.Open);
+            Stream write = new FileStream(destinationFile, FileMode.Append);
+            int c;
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            while ((c = stream.Read(buffer, 0, buffer.Length)) > 0)
+                write.Write(buffer, 0, c);
+
+            stream.Close();
+            write.Close();
+
+            File.Delete(partPath);
             return false;
         }
 
         private static String downloadPart(String uri)
         {
-            return null;
+            Program.wc.DownloadFile(uri, $"{Directory.GetCurrentDirectory()}\\Vidstreaming.part");
+            return $"{Directory.GetCurrentDirectory()}\\Vidstreaming.part";
         }
 
-        public static void m3u8Test(string mfl)
+        private static void m3u8Test(string mfl)
         {
             Console.WriteLine(GetM3u8Link(mfl));
         }
 
-        public static void regTest(string str)
+        private static void regTest(string str)
         {
             MatchCollection ma = reg.Matches(str);
             Console.WriteLine($"{reg.ToString()} : {ma.Count}");
