8e798b5d03d5caec66d8fcf1984fa442bf3cd214
Chay <visionistprojects@gmail.com>
2020-07-29 20:41:05 -0400

    Storage

---
+++ CS/Storage.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace VidStreamIORipper
+{
+    public static class Storage
+    {
+        public static HttpClient client;
+        public static WebClient wc;
+        public static String Aniname; 
+    }
+}
