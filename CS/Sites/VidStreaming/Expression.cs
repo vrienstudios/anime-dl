2dff4d60d5698b4751c1a7f6fa3f71d3c3a088b8
Chay <visionistprojects@gmail.com>
2020-07-29 20:42:32 -0400

    Create Expression.cs

---
+++ CS/Sites/VidStreaming/Expression.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VidStreamIORipper.Sites.VidStreaming
+{
+    public static class Expressions
+    {
+        public static Regex reg = new Regex(@"(sub\..*?\..*?\.m3u8)");
+        public static Regex dwnldLink = new Regex("(?<=\"file\":\")(.+?)(?=\")");
+        public static MatchCollection match;
+
+        public static Regex vidStreamRegex;
+        public static String downloadLinkRegex = "(?<=\"file\":\")(.+?)(?=\")";
+        public static String searchVideoRegex = "<A href=\"(.*)\">"; // Don't say anything about parsing html with REGEX. This is a better than importing another library for this case.
+        public static String videoIDRegex = @"(?<=streaming\.php\?id\=)(.+?)(?=&)";
+    }
+}
