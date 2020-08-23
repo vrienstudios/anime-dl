8d88ce7e678300c2c22d8f63eebe294016dcbf46
Chay <visionistprojects@gmail.com>
2020-08-23 00:42:21 -0400

    Update Extensions.cs

--- CS/Extensions.cs
+++ CS/Extensions.cs
@@ -3,12 +3,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace VidStreamIORipper
 {
     public static class Extensions
     {
+        public static bool IsMp4(string lnk) => lnk.Contains(".mp4");
+
+        public static string getBase64Uri(string xe) => FixUri(Encoding.UTF8.GetString(Convert.FromBase64String(xe)));
+
+        public static int indexOfEquals(string id)
+        {
+            for (int idx = 0; idx < id.Length; idx++)
+                if (id[idx] == '=')
+                    return idx;
+            return -1;
+        }
+
+        public static string FixUri(string uri)
+        {
+            string tes = "storage";
+            for(int idx = 0; idx < uri.Length; idx++)
+            {
+                if (sString(uri, idx, (tes.Length)) == tes)
+                {
+                    return $"https://{uri.Substring(idx, uri.Length - idx)}";
+                }
+            }
+            return null;
+        }
+
+        public static string sString(string b, int i, int x)
+        {
+            Char[] ca = new char[0];
+            int d = 0;
+            while(d < x)
+            {
+                ca = ca.push_back(b[i + d]);
+                d++;
+                if (d > x)
+                    break;
+            }
+            return new string(ca);
+        }
+
         public static String TrimIntegrals(this String str)
         {
             int charsRemoved = 0;
@@ -20,6 +60,26 @@ namespace VidStreamIORipper
                     break;
             }
             return str.Substring(0, str.Length - charsRemoved);
+        }
+
+        public static string ExtractEpisodeNumber(string ep)
+        {
+            bool enNum = false;
+            Char[] numbers = new char[0];
+            ep = ep.Split('.')[1];
+            for(int idx = ep.Length - 1; idx > 0; idx--)
+            {
+                if (char.IsDigit(ep[idx]))
+                {
+                    if (!enNum)
+                        enNum = true;
+                    numbers = numbers.push_back(ep[idx]);
+                }
+                else if(enNum && !char.IsDigit(ep[idx]))
+                    return new string(numbers);
+            }
+            Array.Reverse(numbers);
+            return new string(numbers);
         }
 
         public static string TrimToSlash(this String str)
@@ -42,6 +102,39 @@ namespace VidStreamIORipper
         {
             Char[][] cAT = new char[charArray.Length + 1][];
             for(uint i = 0; i < charArray.Length; i++)
+            {
+                cAT[i] = charArray[i];
+            }
+            cAT[cAT.Length - 1] = value;
+            return cAT;
+        }
+
+        public static Char[] push_back(this Char[] charArray, char value)
+        {
+            Char[] cAT = new char[charArray.Length + 1];
+            for (uint i = 0; i < charArray.Length; i++)
+            {
+                cAT[i] = charArray[i];
+            }
+            cAT[cAT.Length - 1] = value;
+            return cAT;
+        }
+
+        public static object[] push_back(this object[] charArray, object value)
+        {
+            object[] cAT = new object[charArray.Length + 1];
+            for (uint i = 0; i < charArray.Length; i++)
+            {
+                cAT[i] = charArray[i];
+            }
+            cAT[cAT.Length - 1] = value;
+            return cAT;
+        }
+
+        public static Thread[] push_back(this Thread[] charArray, Thread value)
+        {
+            Thread[] cAT = new Thread[charArray.Length + 1];
+            for (uint i = 0; i < charArray.Length; i++)
             {
                 cAT[i] = charArray[i];
             }
