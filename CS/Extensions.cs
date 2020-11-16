346fe352b101c99115050b1baf9a0500aa687a3c
ShujianDou <visionistprojects@gmail.com>
2020-11-16 11:49:25 -0500

    case for spaces

--- CS/Extensions.cs
+++ CS/Extensions.cs
@@ -147,7 +147,7 @@ namespace VidStreamIORipper
             StringBuilder sb = new StringBuilder();
             sb.Capacity = str.Length;
             foreach (char c in str)
-                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_' || c == ' ')
                     sb.Append(c);
             return sb.ToString();
         }
