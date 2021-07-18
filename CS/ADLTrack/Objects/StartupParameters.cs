e427cfc1f415248263385f79089e323c064be324
Chay <13159328+ShujianDou@users.noreply.github.com>
2021-07-17 23:31:16 -0400

    finally working on trackers.

---
+++ CS/ADLTrack/Objects/StartupParameters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADLTrack.Objects
+{
+    public class StartupParameters
+    {
+        //The invterval (in days) for checking if an anime has new episodes.
+        public int updateInterval;
+
+        //The interval (in hours) when the program starts and performs "startupRoutine".
+        public int runInterval;
+
+        public bool isWindows;
+
+        //Track what episode the user is on if true (this requires starting the tracker at boot)
+        public bool trackCurrentEpisodes;
+    }
+}
---
+++ CS/ADLTrack/main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADLTrack
+{
+    public static class main
+    {
+        public static void InitNewStartupRoutine(Objects.StartupParameters params)
+        {
+
+        }
+    }
+}
