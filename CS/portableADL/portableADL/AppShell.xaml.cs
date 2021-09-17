974453e10da912d74ebb4c9f64d34f277723d30e
Chay <visionistprojects@gmail.com>
2021-09-17 15:18:53 -0400

    mobile -> portable rework

---
+++ CS/portableADL/portableADL/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using portableADL.ViewModels;
+using portableADL.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace portableADL
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ CS/portableADL/portableADL/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using portableADL.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace portableADL.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
