38206dbe3ce96d7e96423f59b9b0775aadf4ac18
lucia7777 <38116582+qinatsu@users.noreply.github.com>
2021-10-13 15:14:20 -0500

    Revert "Add Credits Page"
    This reverts commit 1ef641b27c130376209b491c036e821d548798f1.

--- CS/ADLCore/Manga/Models/Manga.cs
+++ CS/ADLCore/Manga/Models/Manga.cs
@@ -18,7 +18,6 @@ namespace ADLCore.Manga.Models
             Epub.Epub e = new Epub.Epub(metaData.name, metaData.author, new Epub.Image() { bytes = metaData.cover });
             int id = 0;
 
-            e.AddPage(CreditsPage());
             foreach (MangaChapter chapter in Chapters)
             {
                 using (StreamReader sr = new StreamReader(zapive.GetEntry("Chapters/" + chapter.ChapterName)?.Open() ?? throw new InvalidOperationException()))
@@ -37,22 +36,6 @@ namespace ADLCore.Manga.Models
             }
             e.CreateEpub(new OPFMetaData(this.metaData.name, this.metaData.author, "Chay#3670", "null", DateTime.Now.ToString()));
             e.ExportToEpub(location);
-        }
-
-        private Page CreditsPage()
-        {
-            List<TiNode> tiNodes = new List<TiNode>();
-            tiNodes.Add(new TiNode { img = new Image[] { Image.GenerateImageFromByte(metaData.cover, "creditsImage") } });
-            tiNodes.Add(new TiNode { text = $"\nHello everyone! Hopefully you have a grand ol' read, but before you do, please read some of these credits." });
-            tiNodes.Add(new TiNode { text = $"" });
-            tiNodes.Add(new TiNode { text = $"Title: " + metaData.name });
-            tiNodes.Add(new TiNode { text = "Author: " + metaData.author });
-            tiNodes.Add(new TiNode { text = $"Chapters {Chapters[0].ChapterName}-{Chapters[Chapters.Length - 1].ChapterName}" });
-            tiNodes.Add(new TiNode { text = $"\nDownloaded from: <a href=\"{metaData.url}\">{metaData.url}</a>", ignoreParsing = true});
-            tiNodes.Add(new TiNode { text = $"\nDownloaded with: <a href=\"https://github.com/vrienstudios/anime-dl\">https://github.com/vrienstudios/anime-dl</a>", ignoreParsing = true});
-            tiNodes.Add(new TiNode { text = $"Requests and questions can be done through our github, my twitter (@shujiandou), or my Discord Chay#3670"});
-            tiNodes.Add(new TiNode { text = $"\n~~ShuJianDou"});
-            return Page.AutoGenerate(tiNodes, "Important");
         }
 
         public void ExportMetaData(ref ZipArchive zip)
---
+++ CS/anime-dl_Android/arrWrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace anime_dl_Android
+{
+    public class arrWrapper<T>
+    {
+        Action<int, string> onIndexSet;
+
+        T[] array;
+
+        public arrWrapper(int size, Action<int, string> u)
+        {
+            array = new T[size];
+            onIndexSet = u;
+        }
+
+        public T this[int id]
+        {
+            set
+            {
+                array[id] = value;
+                onIndexSet(id, value.ToString());
+            }
+            get => array[id];
+        }
+    }
+}
