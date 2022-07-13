0742b8cb0511056e838bfb059d50ba375cd64c5c
ShujianDou <visionistprojects@gmail.com>
2022-07-13 14:40:44 -0400

    fix dir

--- src/Types/ArgumentObject.nim
+++
@@ -1,8 +0,0 @@
-type ArgumentObject* = ref object of RootObj
-    term: string
-    download: bool
-    search: bool
-    toSkip: bool
-    isContinuous: bool
-    isRanged: bool
-    downloadRange: array[2, int]
