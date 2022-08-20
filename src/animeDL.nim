e36c8ba5c6cf59ee3a0052387c705a0ababe5e8b
ShujianDou <visionistprojects@gmail.com>
2022-08-20 00:05:30 -0400

    remove pub-id

--- src/animeDL.nim
+++ src/animeDL.nim
@@ -96,8 +96,7 @@ block:
     let mdataList: seq[metaDataList] = @[
       (metaType: MetaType.dc, name: "title", attrs: @[("id", "title")], text: novelObj.metaData.name),
       (metaType: MetaType.dc, name: "creator", attrs: @[("id", "creator")], text: novelObj.metaData.author),
-      (metaType: MetaType.dc, name: "language", attrs: @[], text: "?"),
-      (metaType: MetaType.dc, name: "identifier", attrs: @[("id", "pub-id")], text: ""),
+      (metaType: MetaType.dc, name: "language", attrs: @[], text: "en"),
       (metaType: MetaType.meta, name: "", attrs: @[("property", "dcterms:modified")], text: "2022-01-02T03:50:100"),
       (metaType: MetaType.dc, name: "publisher", attrs: @[], text: "animedl")]
     var epub3: EPUB3 = CreateEpub3(mdataList, "./" & novelObj.metaData.name)
